8c0111230c7a89ae9e8e2de1a2d7e3428cb27a3b
Freeman Fang <freeman.fang@gmail.com>
2019-10-09 15:09:32 -0400

    add cxf jaxrs quarkus extensio-make integration test working

--- integration-tests/cxf-jaxrs/src/test/java/io/quarkus/cxf/jaxrs/it/CxfJaxrsTest.java
+++ integration-tests/cxf-jaxrs/src/test/java/io/quarkus/cxf/jaxrs/it/CxfJaxrsTest.java
@@ -1,5 +1,8 @@
 package io.quarkus.cxf.jaxrs.it;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -8,8 +11,12 @@ import io.quarkus.test.junit.QuarkusTest;
 class CxfJaxrsTest {
 
     @Test
-    public void test() {
-        //Assertions.fail("Add some assertions to " + getClass().getName());
+    void testEndpoint() {
+        given()
+                .when().get("/cxf-jaxrs/sayHello/freeman")
+                .then()
+                .statusCode(200)
+                .body(containsString("Hello , Welcome to CXF RS Quarkus World!!!"));
     }
 
 }
