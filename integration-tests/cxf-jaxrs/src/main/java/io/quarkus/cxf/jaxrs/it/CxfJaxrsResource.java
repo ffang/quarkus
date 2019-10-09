74407a10e6eb1970a618a179e483dcd83afaab91
Freeman Fang <freeman.fang@gmail.com>
2019-10-09 15:09:32 -0400

    add cxf jaxrs quarkus extensio-make integration test working

--- integration-tests/cxf-jaxrs/src/main/java/io/quarkus/cxf/jaxrs/it/CxfJaxrsResource.java
+++ integration-tests/cxf-jaxrs/src/main/java/io/quarkus/cxf/jaxrs/it/CxfJaxrsResource.java
@@ -17,12 +17,20 @@
 package io.quarkus.cxf.jaxrs.it;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-@Path("/cxf-jaxrs")
+@Path("/sayHello")
 @ApplicationScoped
 public class CxfJaxrsResource {
 
-    // add some rest methods here
+    @GET
+    @Path("/{a}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello(String a) {
+        return "Hello " + a + ", Welcome to CXF RS Quarkus World!!!";
+    }
 
 }
